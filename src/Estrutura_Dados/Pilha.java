237a174a36c11b8ace7a885f647c3ab3f53c45ee
jpCaglianone <joao.caglianone@gmail.com>
2025-02-20 23:35:29 -0300

    inclusao de stack

---
+++ src/Estrutura_Dados/Pilha.java
@@ -0,0 +1,25 @@
+package Estrutura_Dados;
+import java.util.Stack;
+
+public class Pilha {
+
+    public static void StackExemplo() {
+
+        Stack<Integer> pilha = new Stack<>();
+        pilha.push(10);        // Adiciona no topo
+        pilha.pop();           // Remove e retorna o topo
+        pilha.peek();          // Visualiza o topo
+        pilha.empty();         // Verifica se está vazia
+        pilha.search(10);      // Busca elemento (retorna posição)
+        pilha.add(1, 20); // insere no indice (primeiro parametro) um dado valor (segundo parametro)
+        pilha.get(3);       //pega o valor de um dado indice passado como parametro
+        pilha.size();       //tamanho da pilha
+        pilha.isEmpty();    // retorna true se estiver vazia
+        pilha.forEach(System.out::println);
+
+
+        pilha.clear();  //limpa totalmente a pilha
+
+
+    }
+}

bc996ce757df24333abe2978a1d0027ed9514ddd
jpCaglianone <joao.caglianone@gmail.com>
2025-02-25 22:20:02 -0300

    inclusao de polimorfia na HerancaFilho

--- src/POO/HerancaFilho.java
+++ src/POO/HerancaFilho.java
@@ -16,9 +16,22 @@ public class HerancaFilho extends HerancaPai {
 
     public HerancaFilho() {}
 
+
+    //override é uma anotação, sendo a sobrecarga de um metodo, ou seja,
+    // refere-se à capacidade de objetos de classes diferentes responderem ao mesmo método de maneiras distintas.
+    // nao so o override como tambem varios metodos com o mesmo nome podem ser polimorficos
     @Override
     public void apresentar() {
         String ativado = ativo ? "Ativo" : "Desativado";
         System.out.println("Nome: " + nome + "\nIdade: " + idade + "\nHabilidade: " + habilidade + "\nHabilidade: " + ativado);
     }
+
+    //ambos metodos abaixo tambem sao polimorficos
+    public Double salario(double valor, int horas) {
+        return valor * horas;
+    }
+    public Double salario(double valor, int horas, double fatorDesempenho) {
+        return valor * horas * fatorDesempenho;
+    }
+
 }
